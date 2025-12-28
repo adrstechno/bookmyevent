bbd3430bbaf14506f86d6ccf9a7aeeced5cf425d
sparsh-15 <sparshsahu8435@gmail.com>
2025-12-28 15:29:09 +0530

    added headers

--- Event_backend/Server.js
+++ Event_backend/Server.js
@@ -35,6 +35,8 @@ app.use(cors({
     "https://goeventify.com"
   ],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 
